199efc2d1f46d3e7aa54222e8ba0a62a391dc9da
devolskii <devolskii@gmail.com>
2026-05-27 22:24:04 +0530

    fix footer size

--- blpi-fe/components/Footer.tsx
+++ blpi-fe/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="bg-[#DB261D] mt-3">
-      <div className="flex justify-center py-3">
+    <footer className="bg-[#DB261D] mt-3 text-xs">
+      <div className="flex justify-center pt-2 pb-1">
         <p className="text-white">
           &#127279; Made by{" "}
           <a
